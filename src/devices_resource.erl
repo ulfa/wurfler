429f6f2616b9f17da3153f1a98c577e422b6e54d
Ulf <uaforum1@googlemail.com>
2010-11-24 19:08:56 +0100

    extracted the xml stuff from device in xml_factory

--- src/devices_resource.erl
+++ src/devices_resource.erl
@@ -13,7 +13,7 @@
 %%
 %% Exported Functions
 %%
--export([init/1, to_html/2, to_text/2, to_xml/2, content_types_provided/2, resource_exists/2]).
+-export([init/1, to_json/2, to_text/2, to_xml/2, content_types_provided/2, resource_exists/2]).
 -compile([export_all]).
 -include_lib("../deps/webmachine/include/webmachine.hrl").
 -record(context, {device}).
@@ -25,28 +25,31 @@ init([]) ->
 	{ok, #context{device=undefined}}.
 
 content_types_provided(ReqData, Context) ->
-    {[{"text/html", to_html},{"text/plain",to_text}, {"text/xml", to_xml}],ReqData, Context}.
+    {[{"application/json", to_json},{"text/plain",to_text}, {"text/xml", to_xml}],ReqData, Context}.
 
 allowed_methods(ReqData, Context) ->
-    {['GET'], ReqData, Context}.
+    {['POST'], ReqData, Context}.
 
 to_text(ReqData, #context{device=Device}=Context) ->
 	Body=Device#device.id,
     {Body, ReqData, Context}.
 
-to_html(ReqData, Context) ->
-    {Body, _RD, Ctx2} = to_text(ReqData, Context),
-    HBody = io_lib:format("<html><body>~s</body></html>~n",[erlang:iolist_to_binary(Body)]),
-    {HBody, ReqData, Ctx2}.
+to_json(ReqData, Context) ->  
+	error_logger:info_msg("to_json ~n"),
+    {"HBody", ReqData, Context}.
 
 to_xml(ReqData, #context{device=Device}=Context)->
 	{ok, ReqData, Context}.
 
 resource_exists(ReqData, Context) ->
-	true.
-
-
+	error_logger:info_msg("Capabilies : ~p~n", [wrq:req_body(ReqData)]),
+	case get_device(wrq:req_body(ReqData)) of
+		[] -> {false, ReqData, Context};
+		Devices -> {true, ReqData, Context#context{device=Devices}}
+	end.
 %%
 %% Local Functions
 %%
+get_device(Capabilities) ->
+	wurfler:searchByCapabilities(Capabilities).
 

ddd8302722e540da9f9b80cc67097df4424a7923
Ulf <uaforum1@googlemail.com>
2011-01-13 16:55:32 +0100

    added deleted field

--- include/wurfler.hrl
+++ include/wurfler.hrl
@@ -1,7 +1,7 @@
 -define(DEFAULT_TIMESTAMP, "01.01.1970").
 -record(capability, {name, value}).
 -record(group, {id, capabilites=[]}).
--record(device, {id, user_agent, actual_device_root, fall_back, brand_name=undefined, model_name=undefined, groups=[], created, lastmodified}).
+-record(device, {id, user_agent, actual_device_root, fall_back, brand_name=undefined, model_name=undefined, groups=[], created, lastmodified, deleted=false}).
 -record(index, {device, table=[]}).
 -record(brand_index, {brand_name, models=[]}).
 -record(capabilities_devices, {capabilities, devices, created}).

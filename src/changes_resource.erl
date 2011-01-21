6cab690257f6b72fc857992fbad48682020fc7e2
Ulf <uaforum1@googlemail.com>
2011-01-21 17:45:04 +0100

    added change service for receiving updated or new devices

---
+++ src/changes_resource.erl
@@ -0,0 +1,70 @@
+%% Copyright 2010 Ulf
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%% 
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%% 
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+
+%%% -------------------------------------------------------------------
+%%% Author  : Ulf uaforum1@googlemail.com
+%%% Description :
+%%%
+%%% Created : 
+%%% -------------------------------------------------------------------
+-module(changes_resource).
+
+%% --------------------------------------------------------------------
+%% Include files
+%% --------------------------------------------------------------------
+-include_lib("eunit/include/eunit.hrl").
+-include("../include/wurfler.hrl").
+%% --------------------------------------------------------------------
+%% External exports
+%% --------------------------------------------------------------------
+-export([init/1, to_xml/2, to_html/2, content_types_provided/2, allowed_methods/2, resource_exists/2]).
+-include_lib("../deps/webmachine/include/webmachine.hrl").
+%% --------------------------------------------------------------------
+%% record definitions
+%% --------------------------------------------------------------------
+-record(context, {changes=[]}).
+%% --------------------------------------------------------------------
+%%% Internal functions
+%% --------------------------------------------------------------------
+init(_Config) -> 
+%% 	{{trace, "/tmp"}, #context{device=[]}}.
+	{ok, #context{changes=[]}}.
+
+content_types_provided(ReqData, Context) ->
+    {[{"text/xml", to_xml}, {"text/html", to_html}],ReqData, Context}.
+
+allowed_methods(ReqData, Context) ->
+    {['GET'], ReqData, Context}.
+
+to_html(ReqData, Context) ->
+     {ok, Content} = about_dtl:render([]),
+     {Content, ReqData, Context}.
+
+to_xml(ReqData, Context) ->
+    {[], ReqData, Context}.
+
+resource_exists(ReqData, Context) ->
+	Result = find_changes(wrq:path_info(timestamp, ReqData)),
+	{true, ReqData, Context#context{changes=Result}}.
+	
+%% --------------------------------------------------------------------
+%%% Internal functions
+%% --------------------------------------------------------------------
+find_changes(undefined) ->
+	[];
+find_changes(Timestamp) ->
+	wurfler_update:find_changes(Timestamp).
+%% --------------------------------------------------------------------
+%%% Test functions
+%% --------------------------------------------------------------------

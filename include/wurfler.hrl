1cded2d5d25a0f5f4c863c832f4337ea0ad9c81b
Ulf <uaforum1@googlemail.com>
2011-02-13 07:25:56 +0100

    add new field filter to the devices.

--- include/wurfler.hrl
+++ include/wurfler.hrl
@@ -3,7 +3,7 @@
 -define(HOST, "host").
 -record(capability, {name, value}).
 -record(group, {id, capabilites=[]}).
--record(device, {id, user_agent, actual_device_root=false, fall_back, brand_name=undefined, model_name=undefined, groups=[], created, lastmodified}).
+-record(device, {id, user_agent, actual_device_root=false, fall_back, brand_name=undefined, model_name=undefined, groups=[], created, lastmodified, filter}).
 -record(index, {device, table=[]}).
 -record(brand_index, {brand_name, models=[]}).
 -record(capabilities_devices, {capabilities=[], devices=[], key,  created=wurfler_date_util:get_uc_time(), lastmodified=wurfler_date_util:get_uc_time()}).
